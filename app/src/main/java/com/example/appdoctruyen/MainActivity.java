7399116e1ce9d10accd99722c40a1f0adcc66313
babydanh <Macter.970@gmail.com>
2025-05-20 19:32:48 +0700

    botom navigation

--- app/src/main/java/com/example/appdoctruyen/MainActivity.java
+++ app/src/main/java/com/example/appdoctruyen/MainActivity.java
@@ -7,18 +7,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.appdoctruyen.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment()); // Gọi trong onCreate()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            }
+
+            if (id == R.id.search) {
+                replaceFragment(new SearchFragment());
+                return true;
+            }
+
+            if (id == R.id.favor) {
+                replaceFragment(new FavorFragment());
+                return true;
+            }
+
+            if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+                return true;
+            }
+
+            return false;
+        });
+
     }
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, fragment) // ID của FrameLayout trong activity_main.xml
+                .commit();
+    }
+
 }
+
